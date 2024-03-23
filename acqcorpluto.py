5cc1b94d7a47d0befb5acf5f76222386cbdd2bf5
VU3VWB <vu3vwb@gmail.com>
2024-03-23 16:32:42 +0800

    Add correlated data acq and hdf5 read codes

--- acqcorpluto.py
+++ acqcorpluto.py
@@ -20,13 +20,6 @@ parser.add_argument('-NFFT', nargs='?', default=512, type=int, help='Number of f
 
 # args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 args = parser.parse_args()
-
-if args.N is None:
-    Nacq = int(100) #Some number > 1 should work
-    acq_loop_increment = 0
-else:
-    Nacq = int(args.N)-1 # As one acq will happen always
-    acq_loop_increment = 1
     
 import adi
 import numpy as np
@@ -35,8 +28,12 @@ import sys
 import h5py
 import time
 
-def correlate_pluto(buff, NFFT, chunksize):
-    Nsamp = int(chunksize)
+if args.N is None:
+    Nacq = np.inf #Some number > 1 should work
+else:
+    Nacq = int(args.N)-1 # As one acq will happen always
+
+def correlate_pluto(buff, NFFT):
 
     auto11 = np.zeros(NFFT, dtype=np.complex64)
     auto22 = np.zeros(NFFT, dtype=np.complex64)
@@ -44,6 +41,7 @@ def correlate_pluto(buff, NFFT, chunksize):
 
     a = buff[0][:]
     b = buff[1][:]
+    Nsamp = int(buff.shape[1])
 
     for j in range(int(Nsamp/NFFT)):
         c1_fft = np.fft.fft(a[j*NFFT:(j+1)*NFFT])
@@ -57,31 +55,30 @@ def correlate_pluto(buff, NFFT, chunksize):
     cross12 = np.fft.fftshift(cross12)
     return auto11, auto22, cross12
 
-sdr = adi.ad9361("ip:192.168.2.5")
-sdr.rx_enabled_channels = [0, 1]
-sdr.sample_rate = int(args.fs)
-sdr.rx_rf_bandwidth = int(args.fs)
-sdr.rx_lo = int(args.fc)
-sdr.rx_buffer_size = int(args.c)
-sdr.gain_control_mode_chan0 = "manual"
-sdr.gain_control_mode_chan1 = "manual"
-sdr.rx_hardwaregain_chan0 = int(args.g1)
-sdr.rx_hardwaregain_chan1 = int(args.g2)
-
-NFFT = int(args.NFFT)
-fs = int(args.fs)
-fc = int(args.fc)
-freqs = np.fft.fftshift(np.fft.fftfreq(NFFT, 1/fs)+fc)
-
 try:
+    sdr = adi.ad9361("ip:192.168.2.5")
+    sdr.rx_enabled_channels = [0, 1]
+    sdr.sample_rate = int(args.fs)
+    sdr.rx_rf_bandwidth = int(args.fs)
+    sdr.rx_lo = int(args.fc)
+    sdr.rx_buffer_size = int(args.c)
+    sdr.gain_control_mode_chan0 = "manual"
+    sdr.gain_control_mode_chan1 = "manual"
+    sdr.rx_hardwaregain_chan0 = int(args.g1)
+    sdr.rx_hardwaregain_chan1 = int(args.g2)
+
+    NFFT = int(args.NFFT)
+    fs = int(args.fs)
+    fc = int(args.fc)
+    freqs = np.fft.fftshift(np.fft.fftfreq(NFFT, 1/fs)+fc)
     timestr = time.strftime("%Y%m%d-%H%M%S")
     if args.s is None:
         h5_file = str(timestr)+"_plutospec.h5"
     else:
         h5_file = str(timestr)+"_plutospec_"+str(args.s)+".h5"
 
-    print ("Writing to",h5_file)
     hf = h5py.File(h5_file, 'w')
+    print ("Writing to",h5_file)
     data_group = hf.create_group('data')
     data_group.attrs["fc"] = int(args.fc)
     data_group.attrs["fs"] = int(args.fs)
@@ -90,10 +87,12 @@ try:
     data_group.attrs["g2"] = int(args.g2)
     data_group.attrs["NFFT"] = int(args.NFFT)
 
-    timestamp = time.time() #Work in progress
-    samples = sdr.rx() # First RX
-    # print (np.array(samples).shape)
-    auto11, auto22, cross12 = correlate_pluto(buff=samples, NFFT=NFFT, c=args.c)
+    timestamp = time.time()
+    samples = np.array(sdr.rx()) # First RX
+    print (samples.shape)
+
+    auto11, auto22, cross12 = correlate_pluto(buff=samples, NFFT=NFFT)
+
     data_group.create_dataset('timestamps', data=np.array([timestamp]), maxshape=(None,))
     data_group.create_dataset('auto11', data=np.array([auto11]), maxshape=(None, int(NFFT)))
     data_group.create_dataset('auto22', data=np.array([auto22]), maxshape=(None, int(NFFT)))
@@ -102,7 +101,9 @@ try:
     acq_index = 0
     while acq_index < Nacq:
         timestamp = time.time()
-        samples = sdr.rx()
+        print (acq_index, timestamp)
+        samples = np.array(sdr.rx())
+
         auto11, auto22, cross12 = correlate_pluto(buff=samples, NFFT=NFFT)
 
         len_old_data = data_group['timestamps'].shape[0]
@@ -118,9 +119,10 @@ try:
         hf['data/cross12'].resize((len_old_data + 1), axis=0)
         hf['data/cross12'][-1:] = np.array(cross12)
 
-        acq_index = acq_index+acq_loop_increment
-
+        acq_index = acq_index+1
+    print ("Acq completed, exiting")
 except KeyboardInterrupt:
+    print ("KeyboardInterrupt, exiting")
     hf.close()
     sys.exit(130)
 finally:
